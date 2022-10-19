a55fab2f29997a824a850859fd0b99ad4ff31310
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-19 00:08:44 +0000

    added the a table for the total producgt and catergory for display

--- app/controllers/admin/dashboard_controller.rb
+++ app/controllers/admin/dashboard_controller.rb
@@ -1,5 +1,7 @@
 class Admin::DashboardController < ApplicationController
   http_basic_authenticate_with name: ENV['USERNAME'], password: ENV['PASSWORD']
   def show
+    @total_products = Product.all.count
+    @total_categories = Category.all.count
   end
 end
