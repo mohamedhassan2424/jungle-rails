ddef5a16953283cb5365d81cc1c146b7710847d8
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-20 15:12:15 +0000

    having issue with messagee

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,11 +1,22 @@
 class Admin::CategoriesController < ApplicationController
     def index 
-        @catergies = Category.order(id: :desc).all
-        
+        @categories = Category.order(id: :desc).all
+
     end
     def new
+        @category= Product.new
     end
     def create
+        @category = Category.new(product_params)
+        if @category.save
+            redirect_to [:admin, :categories], notice: 'Category created!'
+          else
+            render :new
+          end
     end
-
+    def category_params
+        params.require(:category).permit(
+          :name
+        )
+      end
 end
