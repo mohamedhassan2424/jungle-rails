f1b95ca00133bf3c45ed7bd4efb9e5c3731b02db
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-20 16:13:36 +0000

    forgot to add the session controller, fixed it know

--- app/controllers/users_controller.rb
+++ app/controllers/users_controller.rb
@@ -1,26 +1,18 @@
 class UsersController < ApplicationController
-
+  has_secure_password
   def new
   end
 
   def create
-    user = User.find_by_email(params[:email])
-    # If the user exists AND the password entered is correct.
-    if user && user.authenticate(params[:password])
-      # Save the user id inside the browser cookie. This is how we keep the user 
-      # logged in when they navigate around our website.
-      session[:user_id] = user.id
-      redirect_to '/'
-    else
-    # If user's login doesn't work, send them back to the login form.
-      redirect_to '/login'
-    end
+    user = User.new(user_params)
+  if user.save
+    session[:user_id] = user.id
+    redirect_to '/'
+  else
+    redirect_to '/signup'
   end
-
-  def destroy
-    session[:user_id] = nil
-    redirect_to '/login'
   end
+  
   private
 
   def user_params
