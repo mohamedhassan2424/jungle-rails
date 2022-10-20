9b7d76cbb32437af81000222c069e3519ef013e3
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-20 14:38:12 +0000

    fixed the catergory to show each of the catergory name, know fixing the new and create action view

--- app/controllers/admin/categories_controller.rb
+++ app/controllers/admin/categories_controller.rb
@@ -1,9 +1,11 @@
 class Admin::CategoriesController < ApplicationController
     def index 
+        @catergies = Category.order(id: :desc).all
+        
     end
     def new
     end
     def create
     end
-    
+
 end
