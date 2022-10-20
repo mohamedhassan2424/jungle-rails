29677af0d612b11b27f07e1f626a6605557c35c5
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-20 16:02:02 +0000

    followed the tutorial for the authnetication bycrypt rails, having issue permiting the authorrization for certain controllers

--- config/routes.rb
+++ config/routes.rb
@@ -24,7 +24,9 @@ Rails.application.routes.draw do
     
     # This route sends requests to our naked url to the *cool* action in the *gif* controller.
     #root to: 'gif#cool'
-    
+    get '/login' => 'sessions#new'
+    post '/login' => 'sessions#create'
+    get '/logout' => 'sessions#destroy'
     # I've created a gif controller so I have a page I can secure later. 
     # This is optional (as is the root to: above).
     get '/cool' => 'gif#cool'
