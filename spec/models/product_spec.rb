1612acdbaea75aa640180ba1528743f0dd4d61e0
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-24 18:48:56 +0000

    wrote the tes for the quantity and the category with all the error messages asscoaited with it

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,7 +18,7 @@ describe "name validiations" do
       @product = Product.new
       @category = Category.new
       @product.category = @category
-      @product.price_cents = 5
+      @product.price_cents = 5000
       @product.quantity = 4
       @product.name = "Mohamed Hassan"
       @product.save
@@ -30,7 +30,7 @@ describe "name validiations" do
       @product = Product.new
       @category = Category.new
       @product.category = @category
-      @product.price_cents = 5
+      @product.price_cents = 4000
       @product.quantity = 4
       @product.name = nil
       @product.save
@@ -47,7 +47,7 @@ describe "name validiations" do
       @product = Product.new
       @category = Category.new
       @product.category = @category
-      @product.price_cents = 5
+      @product.price_cents = 5000
       @product.quantity = 4
       @product.name = "Mohamed Hassan"
       @product.save
@@ -60,7 +60,7 @@ describe "name validiations" do
       @category = Category.new
       @product.category = @category
       @product.price_cents = nil
-      @product.quantity = 4
+      @product.quantity = 4000
       @product.name = "Mohamed Hassan"
       @product.save
       error_message = @product.errors.full_messages[0]
@@ -68,6 +68,62 @@ describe "name validiations" do
       expect(error_message).to eq("Price cents is not a number")
       
     
+      expect(@product).to be_invalid
+    end
+  end
+  describe "quantity validiations" do
+    it "quantity provided" do 
+      @product = Product.new
+      @category = Category.new
+      @product.category = @category
+      @product.price_cents = 3000
+      @product.quantity = 4
+      @product.name = "Mohamed Hassan"
+      @product.save
+     
+      expect(@product).to be_valid
+    end
+    
+    it "quantity not provide" do 
+      @product = Product.new
+      @category = Category.new
+      @product.category =  @category
+      @product.price_cents = 2000
+      @product.quantity = nil
+      @product.name = "Mohamed Hassan"
+      @product.save
+      error_message = @product.errors.full_messages[0]
+      puts "____________ #{error_message} "
+      expect(error_message).to eq("Quantity can't be blank")
+      expect(@product).to be_invalid
+    end
+  end
+  describe "category validiations" do
+    it "category provided" do 
+      @product = Product.new
+      @category = Category.new
+      @product.category = @category
+      @product.price_cents = 3000
+      @product.quantity = 4
+      @product.name = "Mohamed Hassan"
+      @product.save
+     
+      expect(@product).to be_valid
+    end
+    
+    it "category not provide" do 
+      @product = Product.new
+      @category = Category.new
+      @product.category = nil
+      @product.price_cents = 2000
+      @product.quantity = 4
+      @product.name = "Mohamed Hassan"
+      @product.save
+      error_message = @product.errors.full_messages[0]
+      puts "____________ #{error_message} "
+      expect(error_message).to eq("Category must exist")
+      
+      #expect(@product).to be_valid
       expect(@product).to be_invalid
     end
   end
