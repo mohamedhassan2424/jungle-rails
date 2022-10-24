162c948724e9338dce31fd37edf6026f899a42e2
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-24 19:48:19 +0000

    added the validation to the code, know will get certain error message which we can use te to debug and fix our code

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,8 @@
 class User < ApplicationRecord
     has_secure_password
+
+ validates :name, presence: true, length: { minimum: 4 }
+  validates :email, presence: true, uniqueness: { case_sensitive: false}
+  validates :password, presence: true, length: { minimum: 6 }
+  validates :password_confirmation, presence: true, length: { minimum: 6}
 end
