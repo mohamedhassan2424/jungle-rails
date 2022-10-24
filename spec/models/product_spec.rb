831625a40978ebcdcce6a6aad452cd9cc8a18675
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-24 18:43:20 +0000

    completed the price test

--- spec/models/product_spec.rb
+++ spec/models/product_spec.rb
@@ -18,7 +18,7 @@ describe "name validiations" do
       @product = Product.new
       @category = Category.new
       @product.category = @category
-      @product.price = 5
+      @product.price_cents = 5
       @product.quantity = 4
       @product.name = "Mohamed Hassan"
       @product.save
@@ -30,7 +30,7 @@ describe "name validiations" do
       @product = Product.new
       @category = Category.new
       @product.category = @category
-      @product.price = 5
+      @product.price_cents = 5
       @product.quantity = 4
       @product.name = nil
       @product.save
@@ -38,6 +38,36 @@ describe "name validiations" do
       puts "____________ #{error_message} "
       #expect(@product).to be_valid
       expect(error_message).to eq("Name can't be blank")
+      expect(@product).to be_invalid
+    end
+  end
+
+  describe "price validiations" do
+    it "price provided" do 
+      @product = Product.new
+      @category = Category.new
+      @product.category = @category
+      @product.price_cents = 5
+      @product.quantity = 4
+      @product.name = "Mohamed Hassan"
+      @product.save
+     
+      expect(@product).to be_valid
+    end
+    
+    it "price not provide" do 
+      @product = Product.new
+      @category = Category.new
+      @product.category = @category
+      @product.price_cents = nil
+      @product.quantity = 4
+      @product.name = "Mohamed Hassan"
+      @product.save
+      error_message = @product.errors.full_messages[0]
+      puts "____________ #{error_message} "
+      expect(error_message).to eq("Price cents is not a number")
+      
+    
       expect(@product).to be_invalid
     end
   end
