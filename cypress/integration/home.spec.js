430769e1f43f59a55c69b837a120f5cbf6cabe5c
Mohamed Hassan <hassanmohamed040@gmail.com>
2022-10-25 17:08:48 +0000

    made allot of changes to the cypress file and my configuration in the vagrant file wasn't write fixed than and as well setup two tests

--- cypress/integration/home.spec.js
+++ cypress/integration/home.spec.js
@@ -1,5 +1,11 @@
-describe("First Test", () => {
-it("first test case", ()=>{
-    cy.vist("/")
-})
+describe(("first test"), ()=>{
+    it("visting the homepage", ()=>{
+        cy.visit("/")
+    })
+    it("There is products on the page", () => {
+        cy.get(".products article").should("be.visible");
+      });
+      it("There is 2 products on the page", () => {
+        cy.get(".products article").should("have.length", 2);
+      });
 })
